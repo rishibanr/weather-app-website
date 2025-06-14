c934f00fa31f50cb4ba2f09c02e3210adbabd54a
rishiban91 <rishiban91@users.noreply.github.com>
2025-06-15 03:12:25 +0530

    Just to test

--- src/app.js
+++ src/app.js
@@ -88,5 +88,5 @@ app.get('/*\w', (req, res) => { //
 });
 
 app.listen(port, () => {
-    console.log("Server is running on port " + port);
+    console.log("Server is running on port number " + port);
 });
